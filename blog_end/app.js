48ce84ac411a2ef43d331834ca80f56d92665877
15196004316@163.com <lfy@0916>
2017-05-13 10:49:38 +0800

    add art-template .gitignore

--- blog_end/app.js
+++ blog_end/app.js
@@ -4,16 +4,27 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-let ejs = require('ejs');
+let artTemplate = require('express-art-template');
+let template = require('art-template');
 
 let index = require('./routes/index');
 let users = require('./routes/users');
 
 let app = express();
 
-// view engine setup
+// 使用art-template
+// template.config('base', '');
+// template.config('extname', '.art');
+// app.engine('.art', template.__express);
+// app.set('view engine', 'art');
+
+app.engine('art', require('express-art-template'));
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'production'
+});
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
