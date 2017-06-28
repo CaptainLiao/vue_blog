20237e23975d85aa36b8aef6c453e00f02aafd72
15196004316@163.com <lfy@0916>
2017-06-28 10:23:10 +0800

    vue router lazy load

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -2,20 +2,26 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // 视图页面
-import Index from '@/pages/index'
-import Blog from '@/pages/blog'
-import Project from '@/pages/project'
-import About from '@/pages/about'
-import List from '@/pages/list'
+// import Index from '@/pages/index'
+// import Blog from '@/pages/blog'
+// import Project from '@/pages/project'
+// import About from '@/pages/about'
+// import List from '@/pages/list'
 
 
 Vue.use(Router);
 
-// const Index = resolve => System.import('../pages/index.vue');
-// const Blog = resolve => System.import('../pages/blog.vue');
-// const Project = resolve => System.import('../pages/project.vue');
-// const About = resolve => System.import('../pages/about.vue');
-// const List = resolve => System.import('../pages/list.vue');
+// const Index = r => require.ensuere([], () => r(require('../pages/index.vue')), 'Index')
+// const Blog = r => require.ensuere([], () => r(require('../pages/blog.vue')), 'Blog')
+// const Project = r => require.ensuere([], () => r(require('../pages/project.vue')), 'Project')
+// const About = r => require.ensuere([], () => r(require('../pages/about.vue')), 'About')
+// const List = r => require.ensuere([], () => r(require('../pages/list.vue')), 'List')
+
+const Index = resolve => {require.ensure(['../pages/index.vue'], () => {resolve(require('../pages/index.vue'))})}
+const Blog = resolve => {require.ensure(['../pages/blog.vue'], () => {resolve(require('../pages/blog.vue'))})}
+const Project = resolve => {require.ensure(['../pages/project.vue'], () => {resolve(require('../pages/project.vue'))})}
+const About = resolve => {require.ensure(['../pages/about.vue'], () => {resolve(require('../pages/about.vue'))})}
+const List = resolve => {require.ensure(['../pages/list.vue'], () => {resolve(require('../pages/list.vue'))})}
 
 
 let index = {
